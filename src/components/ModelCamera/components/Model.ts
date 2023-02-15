ec66efe07ec69e230b63e7e2930e76927c21b7df
Angel G. Ortiz <ortizangelg23@gmail.com>
2023-02-15 14:23:07 -0400

    changed model.ts

--- src/components/ModelCamera/components/Model.ts
+++ src/components/ModelCamera/components/Model.ts
@@ -11,26 +11,22 @@ export async function loadModel() {
         tflite.setWasmPath(
             'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-tflite@0.0.1-alpha.8/dist/'
         );
-        console.log('isMobile');
-        result.R_Model = await tflite?.loadTFLiteModel('https://tflite-asl-models.s3.amazonaws.com/R_converted_model.tflite')
-        result.L_Model = await tflite?.loadTFLiteModel('https://tflite-asl-models.s3.amazonaws.com/L_converted_model.tflite')
+
+        result.R_Model = await tflite.loadTFLiteModel('https://tflite-asl-models.s3.amazonaws.com/R_converted_model.tflite')
+        result.L_Model = await tflite.loadTFLiteModel('https://tflite-asl-models.s3.amazonaws.com/L_converted_model.tflite')
     } else {
-        console.log("isDesktop");
-        result.R_Model = await tf?.loadGraphModel(
+
+        result.R_Model = await tf.loadGraphModel(
             "https://signy-asl-models.s3.amazonaws.com/models/R_graph_model/model.json"
         );
-        result.L_Model = await tf?.loadGraphModel(
+        result.L_Model = await tf.loadGraphModel(
             "https://signy-asl-models.s3.amazonaws.com/models/L_graph_model/model.json"
         );
     }
 
-
-
-
-
     return result;
 }
-
+// Next two functions are for preprocessing the landmarks passed to the model
 export function calc_landmark_list(
     landmarks: any,
     width: number,
